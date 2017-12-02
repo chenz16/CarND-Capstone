b9b3a46ea5dfcbde98ad87969631a7520d958006
Brian Tamm <btamm@tam-kor.com>
2017-12-02 12:29:27 -0500

    Corrected BGR/RGB colorspace conversion which was preventing proper classification. Added debugging code to log annotated images.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -5,8 +5,11 @@ import tensorflow as tf
 import numpy as np
 import cv2
 import rospkg
+from PIL import Image
 
+import visualization_utils
 
+# SSD Mobilenet
 CKPT = os.path.join('light_classification', 'frozen', 'frozen_inference_graph.pb')
 CLASS_TO_STATE = {
     1: TrafficLight.RED,
@@ -15,6 +18,22 @@ CLASS_TO_STATE = {
 }
 SSD_SIZE = 600
 SCORE_THRESH = 0.3
+
+# Debugging
+LOG_DIR = '/home/btamm/Documents/udacity/term3/CarND-Capstone_old/results/sim_results'
+CLASSIFICATION_LOG = '/home/btamm/Documents/udacity/term3/CarND-Capstone_old/results/classification_log.txt'
+STATE_TO_COLOR = {
+    TrafficLight.RED: 'Red',
+    TrafficLight.YELLOW: 'Yellow',
+    TrafficLight.GREEN: 'Green',
+    TrafficLight.UNKNOWN: 'Unknown',
+}
+CLASS_DICTS = {int(1): {'id': int(1), 'name': 'RED'},
+               int(2): {'id': int(2), 'name': 'YELLOW'},
+               int(3): {'id': int(3), 'name': 'GREEN'}}
+
+# # Mobilenet
+# CKPT = os.path.join('light_classification', 'frozen', 'mobilenet_tl_frozen.pb')
 
 
 class TLClassifier(object):
@@ -31,6 +50,8 @@ class TLClassifier(object):
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
 
+        self.log_count = 0
+
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
 
@@ -41,6 +62,8 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
         # implement light color prediction
         with self.detection_graph.as_default():
             with tf.Session(graph=self.detection_graph) as sess:
@@ -62,9 +85,27 @@ class TLClassifier(object):
                     [detection_boxes, detection_scores, detection_classes, num_detections],
                     feed_dict={image_tensor: image_expanded})
 
-        total_scores = np.zeros(3, dtype=np.float32)
+        # Remove unnecessary dimensions
+        boxes = np.squeeze(boxes)
         scores = np.squeeze(scores)
-        classes = np.squeeze(classes)
+        classes = np.squeeze(classes).astype(int)
+
+        # create overlay image for debugging
+        draw_img = visualization_utils.visualize_boxes_and_labels_on_image_array(image.copy(),
+                                                  boxes,
+                                                  classes,
+                                                  scores,
+                                                  CLASS_DICTS,
+                                                  use_normalized_coordinates=True,
+                                                  min_score_thresh=SCORE_THRESH)
+        self.log_count += 1
+        fname = 'result_{0:0>5}.jpg'.format(self.log_count)
+        image_file = os.path.join(LOG_DIR, fname)
+        draw_img = Image.fromarray(draw_img)
+        draw_img.save(image_file)
+
+        # get overall prediction
+        total_scores = np.zeros(3, dtype=np.float32)
         if num > 0:
             for cls, score in zip(classes, scores):
                 if score > SCORE_THRESH:
@@ -72,5 +113,10 @@ class TLClassifier(object):
             prediction = CLASS_TO_STATE[np.argmax(total_scores) + 1]
         else:
             prediction = TrafficLight.UNKNOWN
+
+        # log overall classifications
+        data_line = '{}, {}'.format(fname, STATE_TO_COLOR[prediction])
+        with open(CLASSIFICATION_LOG, 'a+') as out_file:
+            out_file.write(data_line + os.linesep)
 
         return prediction
